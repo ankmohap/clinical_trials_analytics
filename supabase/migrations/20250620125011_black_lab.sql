b3f383871a09c5c1a54d62b84b822fc92605a481
ankmohap <43520996+ankmohap@users.noreply.github.com>
2025-06-20 21:43:00 +0530

    delete

--- supabase/migrations/20250620125011_black_lab.sql
+++
@@ -1,5 +0,0 @@
-{% macro test_data_freshness(model, date_column, max_days_old=7) %}
-    select count(*)
-    from {{ model }}
-    where {{ date_column }} < current_date() - {{ max_days_old }}
-{% endmacro %}
--- supabase/migrations/20250620125014_square_mouse.sql
+++
@@ -1,4 +0,0 @@
--- Test to ensure all NCT IDs follow the expected format
-select *
-from {{ ref('stg_clinical_trials') }}
-where not regexp_like(nct_id, '^NCT[0-9]{8}$')
